2228f096c9a7f397654e4ba8aeb27a707b8f8d20
labocho <labocho@penguinlab.jp>
2012-08-23 19:11:34 +0900

    merge multiple line

--- lib/japanese_zip_code/updater.rb
+++ lib/japanese_zip_code/updater.rb
@@ -44,8 +44,10 @@ module JapaneseZipCode
 
       success = 0
       failure = 0
+
+      zip_code = nil
       CSV.foreach(csv, csv_options) do |row|
-        zip_code = ZipCode.new(
+        zip_code_temp = ZipCode.new(
           organization_code: row[0],
           zip5: row[1].strip,
           zip: row[2],
@@ -57,7 +59,30 @@ module JapaneseZipCode
           street: normalize(filter_street(row[8])),
         )
 
-        if zip_code.save
+        if @street_continue || @street_phonetic_continue
+          # 継続行が street / street_phonetic 以外は一致するか調べる
+          # もし一致しなければ継続行との判断はミスなので例外
+          %w(organization_code zip5 zip prefecture_phonetic city_phonetic prefecture city).each do |a|
+            unless zip_code[a] == zip_code_temp[a]
+              raise "Continuous line is invalid:\n  #{zip_code.inspect}\n  #{zip_code_temp.inspect}"
+            end
+          end
+
+          # 漢字表記は分割していても読みは分割していない (すべて同じ値が入る) 場合があるため
+          # 個別に処理する
+          zip_code.street << zip_code_temp.street if @street_continue
+          zip_code.street_phonetic << zip_code_temp.street_phonetic if @street_phonetic_continue
+        else
+          zip_code = zip_code_temp
+        end
+
+        # まだ継続行があるか調べ、あれば next
+        @street_continue = street_continue?(zip_code)
+        @street_phonetic_continue = street_phonetic_continue?(zip_code)
+        next if @street_continue || @street_phonetic_continue
+
+        # もう継続行がないので保存
+        if zip_code.save!
           success += 1
         else
           failure += 1
@@ -67,6 +92,9 @@ module JapaneseZipCode
         if (success + failure) % 1000 == 0
           puts "#{(success + failure)} lines processed."
         end
+        zip_code = nil
+        @street_continue = nil
+        @street_phonetic_continue = nil
       end
 
       puts "Complete! #{success} success. #{failure} failure."
@@ -80,6 +108,20 @@ module JapaneseZipCode
       return if str == "以下に掲載がない場合"
       return if str == "ｲｶﾆｹｲｻｲｶﾞﾅｲﾊﾞｱｲ"
       str
+    end
+
+    # street に () があり、対応がとれていなければ true
+    def street_continue?(zip_code)
+      s = zip_code.street
+      return true if s && (s.gsub(/[^\(]/, "").size != s.gsub(/[^\)]/, "").size)
+      false
+    end
+
+    # street_phonetic に () があり、対応がとれていなければ true
+    def street_phonetic_continue?(zip_code)
+      s = zip_code.street_phonetic
+      return true if s && (s.gsub(/[^\(]/, "").size != s.gsub(/[^\)]/, "").size)
+      false
     end
   end
 end
