180fbea59492246fdc2dd76f0c1dce1a8f0cb8b1
labocho <labocho@penguinlab.jp>
2020-02-21 14:50:20 +0900

    dont load rake tasks when load gem

--- lib/japanese_zip_code.rb
+++ lib/japanese_zip_code.rb
@@ -3,5 +3,4 @@ require "japanese_zip_code/version"
 module JapaneseZipCode
   autoload :Updater, "japanese_zip_code/updater"
   autoload :ZipCode, "japanese_zip_code/zip_code"
-  require "japanese_zip_code/railtie" if defined?(Rails)
 end
